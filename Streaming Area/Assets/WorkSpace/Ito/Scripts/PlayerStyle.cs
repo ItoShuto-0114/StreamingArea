1e06e69db5c8f73b6dd227cab4bb95512cec86c0
shuto20051014 <shuto20051014@gmail.com>
2026-07-20 16:27:08 +0900

    [update]スタイルチェンジの処理の追加

--- Streaming Area/Assets/WorkSpace/Ito/Scripts/PlayerStyle.cs
+++ Streaming Area/Assets/WorkSpace/Ito/Scripts/PlayerStyle.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+public enum StyleType
+{
+    None,
+    Speed,
+    Blade,
+    Berserk
+}
 public class PlayerStyle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public StyleType CurrentStyle { get; private set; } = StyleType.None;
+    void ChangeStyle()
     {
-        
-    }
+         switch(CurrentStyle)
+        {
+            case StyleType.None:
+                CurrentStyle = StyleType.Speed;
+                break;
 
-    // Update is called once per frame
-    void Update()
+            case StyleType.Speed:
+                CurrentStyle = StyleType.Blade;
+                break;
+
+            case StyleType.Blade:
+                CurrentStyle = StyleType.Berserk;
+                break;
+
+            case StyleType.Berserk:
+                CurrentStyle = StyleType.Speed;
+                break;
+        }
+    }
+    public void OnChangeStyle(InputAction.CallbackContext context)
     {
-        
+        if(context.performed)
+        {
+            ChangeStyle();
+        }
     }
 }
